7a647c0e14cc71a4044023c9b07190415c604608
Baptiste <smaug.exe@gmail.com>
2019-02-22 09:56:07 +0100

    add un collegue via form

---
+++ src/main/java/dev/top/controller/CollegueApi.java
@@ -0,0 +1,86 @@
+package dev.top.controller;
+
+public class CollegueApi {
+	
+	private String matricule;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String dateNaissance;
+	private String sexe;
+	private String adresse;
+	private String password;
+	private String photo;
+	
+	public CollegueApi(String matricule, String nom,String prenom, String email,String dateNaissance,String sexe,String adresse,String password, String photo) {
+		
+		this.matricule=matricule;
+		this.nom=nom;
+		this.prenom=prenom;
+		this.email=email;
+		this.dateNaissance=dateNaissance;
+		this.sexe=sexe;
+		this.adresse=adresse;
+		this.password=password;
+		this.photo=photo;
+		
+	}
+	
+	public CollegueApi() {}
+	
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(String dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getSexe() {
+		return sexe;
+	}
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+}
--- src/main/java/dev/top/controller/CollegueCtrl.java
+++ src/main/java/dev/top/controller/CollegueCtrl.java
@@ -2,15 +2,19 @@ package dev.top.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import dev.top.entities.Collegue;
 import dev.top.repos.CollegueRepo;
@@ -20,32 +24,54 @@ import dev.top.repos.CollegueRepo;
 @RequestMapping("/collegues")
 public class CollegueCtrl {
 
-    @Autowired
-    private CollegueRepo collegueRepo;
+	@Autowired
+	private CollegueRepo collegueRepo;
 
-    @GetMapping
-    public List<Collegue> findAll() {
-        return this.collegueRepo.findAll();
-    }
-    
-    @PatchMapping(value = "{pseudo}")
-    public Collegue updatePseudo(@PathVariable String pseudo, @RequestBody Map<String,String> action) {
-    	List<Collegue> collegues = findAll();
-    	System.out.println("TTTTTEST");
-    	for(Collegue c : collegues) {
-    		if(c.getPseudo().equals(pseudo)) {
-    			System.out.println("test"+action);
-    			if(action.get("action").equals("AIMER"))
-    				c.setScore(c.getScore()+1);
-    			else {
-    				if(action.get("action").equals("DETESTER"))
-    					c.setScore(c.getScore()-1);
-    				}    				
-    			this.collegueRepo.save(c);
-    			return c;
-    		}
-    	}
-    	return null;
-    }
-    
+	@GetMapping
+	public List<Collegue> findAll() {
+		return this.collegueRepo.findAll();
+	}
+
+	@PatchMapping(value = "{pseudo}")
+	public Collegue updatePseudo(@PathVariable String pseudo, @RequestBody Map<String, String> action) {
+		List<Collegue> collegues = findAll();
+		for (Collegue c : collegues) {
+			if (c.getPseudo().equals(pseudo)) {
+				if (action.get("action").equals("AIMER"))
+					c.setScore(c.getScore() + 1);
+				else {
+					if (action.get("action").equals("DETESTER"))
+						c.setScore(c.getScore() - 1);
+				}
+				this.collegueRepo.save(c);
+				return c;
+			}
+		}
+		return null;
+	}
+
+	@PostMapping
+	public void postCollegue(@RequestBody Map<String, String> action) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		 
+        // Send request with GET method and default Headers.
+		CollegueApi[] aux = restTemplate.getForObject("https://tommy-sjava.cleverapps.io/collegues", CollegueApi[].class);
+		String matricule = action.get("matricule");
+		boolean aux2=false;
+		for(CollegueApi collegueApi : aux) {
+	        if(collegueApi.getMatricule().equals(matricule)) {
+	        	Collegue c = new Collegue(action.get("pseudo"),0,action.get("URLImage"));
+	        	this.collegueRepo.save(c);
+	        	System.out.println("test1"+c.getPhotoUrl());
+	        	aux2=true;
+	        }
+		}
+		if(!aux2) {
+			System.out.println(action);
+        	Collegue c = new Collegue(action.get("pseudo"),0,action.get("URLImage"));
+        	System.out.println("test2"+c.getPhotoUrl());
+        	this.collegueRepo.save(c);
+		}     
+	}
 }
